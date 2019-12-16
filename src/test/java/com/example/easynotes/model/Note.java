4d193d868bc1a55e9c2e844be8e3fc8a29f3e70f
sb <sergeyboyakov@gmail.com>
2019-12-16 13:50:40 +0300

    Add Note entity and Note repository

--- src/test/java/com/example/easynotes/model/Note.java
+++ src/test/java/com/example/easynotes/model/Note.java
@@ -1,12 +1,47 @@
 package com.example.easynotes.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Date;
 
 @Entity
-public class Note {
+@Table(name = "notes")
+@EntityListeners(AuditingEntityListener.class) // For @CreatedDate and @LastModifiedDate, also @EnableJpaAuditing in main class
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public class Note implements Serializable { // This guy puts 'implements Serializable' cuz this entity is also DTO :(
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @NotBlank
+    String title;
+
+    @NotBlank
+    String content;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date updatedAt;
+
 }
